28ca00e81021fe36d70664207bf84b5c68b2588a
Eniamza <eniamza0@gmail.com>
2023-09-19 02:48:01 +0600

    scheduled commit

--- usisup.bat
+++ usisup.bat
@@ -1,3 +1,16 @@
-git add . 
+@echo off
+
+:: Navigate to your Git repository directory
+cd /d "F:\Hek\usis2global\usis2global"
+
+:: Add all changes
+git add .
+
+:: Commit changes with a commit message
 git commit -m "scheduled commit"
+
+:: Push changes to the remote repository
 git push
+
+:: Optional: Display a message
+echo Git add, commit, and push completed.
