8303e2728ab6648d02aeb77ec98b4dd8f2db6027
eniamza <eniamza0@gmail.com>
2023-09-19 23:10:01 +0000

    Automatic commit on 2023-09-19_23:10:01

---
+++ push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Change to the directory of your Git repository
+cd /root/usis/usis2global
+
+# Check for local changes
+if git diff --quiet; then
+    echo "No local changes."
+else
+    # Commit changes with a timestamp as the commit message
+    git add .
+    git commit -m "Automatic commit on $(date +\%Y-\%m-\%d_\%H:\%M:\%S)"
+    git push origin master
+    echo "Changes committed and pushed."
+fi
+
