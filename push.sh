dc56839cb68166547914c8caf2410b4f0c1a3ef0
eniamza <eniamza0@gmail.com>
2023-09-20 00:10:01 +0000

    Automatic commit on 2023-09-20_00:10:01

--- push.sh
+++ push.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 # Change to the directory of your Git repository
-cd /root/usis/usis2global
+cd /root/usis/usis2global || exit 1
 
 # Check for local changes
 if git diff --quiet; then
     echo "No local changes."
 else
     # Commit changes with a timestamp as the commit message
-    git add .
-    git commit -m "Automatic commit on $(date +\%Y-\%m-\%d_\%H:\%M:\%S)"
-    git push origin master
-    echo "Changes committed and pushed."
+    if git add . && git commit -m "Automatic commit on $(date +\%Y-\%m-\%d_\%H:\%M:\%S)"; then
+        if git push origin master; then
+            echo "Changes committed and pushed."
+        else
+            echo "Failed to push changes." >&2
+        fi
+    else
+        echo "Failed to commit changes." >&2
+    fi
 fi
 
