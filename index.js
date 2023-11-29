da8ec2e73f735cf7333ba784f5e6ad5b67a23ce7
Eniamza <eniamza0@gmail.com>
2023-11-29 20:56:55 +0600

    Fix: Filter out class timing, from Lab timing

--- index.js
+++ index.js
@@ -1,48 +1,45 @@
-async function execute(){
+async function execute() {
         let usisdata = await fetch("https://usis-cdn.eniamza.com/usisdump.json");
-        usisdata = await usisdata.json() 
+        usisdata = await usisdata.json();
         usisdata.sort(function(a, b) {
-                return a.courseCode.localeCompare(b.courseCode);
-             });
-
-        console.log(usisdata[0])
-
-        let table = document.getElementById("table")
-
-        let getFirstObjectKeys = Object.keys(usisdata[0])
-        let wantedHeaderKeys = ["courseDetails","empShortName","defaultSeatCapacity","totalFillupSeat","availableSeat"]
-
+            return a.courseCode.localeCompare(b.courseCode);
+        });
+    
+        console.log(usisdata[0]);
+    
+        let table = document.getElementById("table");
+    
         usisdata.forEach(element => {
 
-                let tr = document.createElement("tr")
+            const classScheduleArray = element.classSchedule.split(/,\s*/);
+            const classLabScheduleArray = element.classLabSchedule.split(/,\s*/);
+    
 
-                let values = [
-                        element.courseDetails,
-                        element.empShortName,
-                        element.defaultSeatCapacity,
-                        element.totalFillupSeat,
-                        element.availableSeat,
-                        element.classSchedule,
-                        element.classLabSchedule
-                ]
-                
-                values.forEach(item => {
-                        let td = document.createElement("td")
-                        td.innerText = String(item).replaceAll(",","\n")
-                        tr.appendChild(td)
-                })
-
-                table.append(tr)
+            const filteredLabSchedule = classLabScheduleArray.filter(labSchedule => {
+                return !classScheduleArray.includes(labSchedule);
+            });
+    
+            let tr = document.createElement("tr");
+    
+            let values = [
+                element.courseDetails,
+                element.empShortName,
+                element.defaultSeatCapacity,
+                element.totalFillupSeat,
+                element.availableSeat,
+                classScheduleArray.join("\n"),
+                filteredLabSchedule.join("\n")
+            ];
+    
+            values.forEach(item => {
+                let td = document.createElement("td");
+                td.innerText = item;
+                tr.appendChild(td);
+            });
+    
+            table.appendChild(tr);
         });
-
-
-        
-
-
-
-
-}
-
-
-
-execute()
+    }
+    
+    execute();
+    
