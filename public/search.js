b5ad15b9c807d9ac12d27411d5fe857c5acea998
eniamza <eniamza0@gmail.com>
2024-09-19 21:48:43 +0600

    Feat: Add faculty search

--- public/search.js
+++ public/search.js
@@ -1,5 +1,5 @@
 function searchTable() {
-    let input, filter, table, tr, td, i, txtValue;
+    let input, filter, table, tr, td1, td2, i, txtValue1, txtValue2;
     input = document.getElementById("searchInput");
     filter = input.value.toUpperCase();
     table = document.getElementById("table");
@@ -7,11 +7,13 @@ function searchTable() {
 
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-        // Change this index if you want to search in a specific column
-        td = tr[i].getElementsByTagName("td")[0]; 
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        // Get the first and second columns
+        td1 = tr[i].getElementsByTagName("td")[0];
+        td2 = tr[i].getElementsByTagName("td")[2];
+        if (td1 || td2) {
+            txtValue1 = td1 ? (td1.textContent || td1.innerText) : "";
+            txtValue2 = td2 ? (td2.textContent || td2.innerText) : "";
+            if (txtValue1.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
             } else {
                 tr[i].style.display = "none";
@@ -24,8 +26,8 @@ let debounceTimeout;
 
 function debounceSearch() {
     clearTimeout(debounceTimeout);
-    debounceTimeout = setTimeout(searchTable, 1); // 1000ms = 1 second
-    window.scrollTo(0,0)
+    debounceTimeout = setTimeout(searchTable, 300); // 300ms debounce time
+    window.scrollTo(0, 0);
 }
 
 document.getElementById('searchInput').addEventListener('keyup', debounceSearch);
